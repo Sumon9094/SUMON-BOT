2a5ca83373d87f1f7bbe1e1d8e373859365d4cc2
SAGOR690beep <jahidul2782466@gmail.com>
2025-05-21 23:59:58 +0600

    Update up.js

--- models/commands/up.js
+++ models/commands/up.js
@@ -1,13 +1,12 @@
-const fbname = "SaikiDesu";
-const githubname = "mraikero-01";
 module.exports.config = {
-	name:"up",
+	name: "up",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "SaGor",
-	description: "Random ảnh theo api - uptime",
-	commandCategory: "Nhóm",
-	cooldowns: 3,
+	credits: "Mirai-Team",
+  hide: true,
+	description: "Random images by api - uptime",
+	commandCategory: "administrators",
+	cooldowns: 2,
   dependencies: {
 		"pidusage": ""
 	}
@@ -18,29 +17,52 @@ function byte2mb(bytes) {
 	while (n >= 1024 && ++l) n = n / 1024;
 	return `${n.toFixed(n < 10 && l > 0 ? 1 : 0)} ${units[l]}`;
 }
-module.exports.run = async ({ api, event, args }) => {
-const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
+module.exports.run = async ({ api, event, args , Threads}) => {
+  var username = process.env.REPL_OWNER
+  if(username !== undefined) {
+    var urlRepl = `https://${process.env.REPL_SLUG}.${username}.repl.co`
+}
+  var os = require("os");
+	var cpus = os.cpus();
+	var chips;
+	for (var i of cpus) chips = i.model, speed = i.speed;
+	if (cpus == undefined);
+var time = process.uptime(),
+        hours = Math.floor(time / (60 * 60)),
+        minutes = Math.floor((time % (60 * 60)) / 60),
+        seconds = Math.floor(time % 60);
   var z_1 = (hours < 10) ? '0' + hours : hours;
     var x_1 = (minutes < 10) ? '0' + minutes : minutes;
     var y_1 = (seconds < 10) ? '0' + seconds : seconds;
     const axios = require('axios')
+    const threadSetting = (await Threads.getData(String(event.threadID))).data || 
+    {};
+    const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX 
+    : global.config.PREFIX;
+    const { commands } = global.client;
 	const pidusage = await global.nodemodule["pidusage"](process.pid);
 	const timeStart = Date.now();
   const fs = require('fs-extra');
-  const moment = require("moment-timezone");
-  const timeNow = moment.tz("Asia/Manila").format("DD/MM/YYYY || HH:mm:ss");
-   if (!fs.existsSync(__dirname + `/tad/Grandstander-Bold.ttf`)) {
-    let getfont2 = (await axios.get(`https://github.com/hanakuUwU/font/raw/main/Grandstander-Bold.ttf`, { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(__dirname + `/tad/Grandstander-Bold.ttf`, Buffer.from(getfont2, "utf-8"));
-  };
+   if (!fs.existsSync(__dirname +
+        `/cache/UTM-Avo.ttf`)) {
+        let getfont = (await axios.get(`https://github.com/quyenkaneki/data/blob/main/UTM-Avo.ttf?raw=true`, { responseType: "arraybuffer" })).data;
+        fs.writeFileSync(__dirname + `/cache/UTM-Avo.ttf`, Buffer.from(getfont, "utf-8"));
+      }
+         if (!fs.existsSync(__dirname +
+      `/cache/phenomicon.ttf`)) {
+      let getfont2 = (await axios.get(`https://github.com/quyenkaneki/data/blob/main/phenomicon.ttf?raw=true`, { responseType: "arraybuffer" })).data;
+      fs.writeFileSync(__dirname + `/cache/phenomicon.ttf`, Buffer.from(getfont2, "utf-8"));
+    };
+  if (!fs.existsSync(__dirname +
+      `/cache/CaviarDreams.ttf`)) {
+      let getfont3 = (await axios.get(`https://github.com/quyenkaneki/data/blob/main/CaviarDreams.ttf?raw=true`, { responseType: "arraybuffer" })).data;
+      fs.writeFileSync(__dirname + `/cache/CaviarDreams.ttf`, Buffer.from(getfont3, "utf-8"));
+    };
    const { loadImage, createCanvas, registerFont } = require("canvas");
   
   let k = args[0];
    if(args[0] == "list"){
-    const alime = (await axios.get('https://raw.githubusercontent.com/mraikero-01/saikidesu_data/main/anilist2.json')).data
+    const alime = (await axios.get('https://raw.githubusercontent.com/quyenkaneki/data/main/dataanime.json')).data
     var count = alime.listAnime.length;
       var data = alime.listAnime
       var page = 1;
@@ -61,87 +83,69 @@ const time = process.uptime(),
   } else {
     var id = k
   }
-    const lengthchar = (await axios.get('https://run.mocky.io/v3/0dcc2ccb-b5bd-45e7-ab57-5dbf9db17864')).data
+    const lengthchar = (await axios.get('https://raw.githubusercontent.com/quyenkaneki/data/main/dataimganime.json')).data
     console.log(lengthchar.length)
-  const Canvas = require('canvas');
-   let pathImg = __dirname + `/tad/avatar_115.png`;
-    let pathAva = __dirname + `/tad/avatar_223.png`;
-    let pathLine = __dirname + `/tad/avatar_343.png`;
-     let pathIcon = __dirname + `/tad/avatar_3ds23c12311.png`;
-    let pathIconIG = __dirname + `/tad/sssss.png`;
-    let pathIcongithub = __dirname + `/tad/owo.png`;
-    let icon3 = (await axios.get(encodeURI(`https://imgur.com/u8UU1cb.png`), { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(pathIcongithub, Buffer.from(icon3, "utf-8"));
-     let icon = (await axios.get(encodeURI(`https://imgur.com/TEAVHvA.png`), { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(pathIcon, Buffer.from(icon, "utf-8"));
-    let icon2 = (await axios.get(encodeURI(`https://imgur.com/pJnSU7m.png`), { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(pathIconIG, Buffer.from(icon2, "utf-8"));
-    let avtAnime = (
-      await axios.get(encodeURI(`${lengthchar[id].imgAnime}`), { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(pathAva, Buffer.from(avtAnime, "utf-8"));
-         let background = (await axios.get(encodeURI(`https://imgur.com/1QncZxH.png`), { responseType: "arraybuffer" })).data;
+    const Canvas = require('canvas');
+    let pathImg = __dirname + `/cache/${id}.png`;
+    let pathAva = __dirname + `/cache/${event.senderID}.png`;
+    let background = (await axios.get(encodeURI(`https://imgur.com/x5JpRYu.png`), { responseType: "arraybuffer" })).data;
     fs.writeFileSync(pathImg, Buffer.from(background, "utf-8"));
- let line = (await axios.get(encodeURI(`https://imgur.com/1iWNkmi.png`), { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(pathLine, Buffer.from(line, "utf-8"));
+    let ava = (await axios.get(encodeURI(`${lengthchar[id].imgAnime}`), { responseType: "arraybuffer" })).data;
+    fs.writeFileSync(pathAva, Buffer.from(ava, "utf-8"));
     const request = require('request');
     const path = require('path');
 
   //const a = Math.floor(Math.random() * 820) + 1
   
   
-let a = await loadImage(pathAva)
-    let a1 = await loadImage(pathImg)
-    let a2 = await loadImage(pathLine)
-    let g = await loadImage(pathIcon);
-    let r = await loadImage(pathIconIG);
-    let o = await loadImage(pathIcongithub);
-    let canvas = createCanvas(a1.width, a1.height);
-    let
-      ctx = canvas.getContext("2d");
+let l1 = await loadImage(pathAva);
+    let a = await loadImage(pathImg);
+    let canvas = createCanvas(a.width, a.height);
+    var ctx = canvas.getContext("2d");
     ctx.fillStyle = lengthchar[id].colorBg;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(a1, 0, 0, canvas.width, canvas.height);
-    ctx.drawImage(a2, 2000, 200, a2.width, a2.height);
-    ctx.drawImage(g, 800, 2500, 350, 350);
-    ctx.drawImage(o, 1100, 2800, 550, 550);
-    ctx.drawImage(r, 1600, 3350, 350, 350);
-    ctx.drawImage(a, 4000, -590, 5300, 5300);
-    Canvas.registerFont(__dirname + `/tad/Grandstander-Bold.ttf`, {
-      family: "Grandstander"
+    ctx.drawImage(l1, -200, -200, 1200, 1200);
+    ctx.drawImage(a, 0, 0, canvas.width, canvas.height);
+     registerFont(__dirname + `/cache/phenomicon.ttf`, {
+      family: "phenomicon"
     });
-    ctx.textAlign = "center";
-    ctx.font = "750px Grandstander";
-    ctx.fillStyle = "#FFF"
-    ctx.fillText("SaGor", canvas.width /  2 - 660, 1600);
-    ctx.save();
-    ctx.font = "350px Grandstander";
-    ctx.fillStyle = "#000"
-    ctx.fillText("SaGor", canvas.width / 2 - 660, 1000);
-    ctx.fillText(`${z_1}:${x_1}:${y_1}`, canvas.width / 2 - 630, 2000);
+    ctx.textAlign = "start";
+    ctx.strokeStyle = lengthchar[id].colorBg;
+    ctx.filter = "brightness(90%) contrast(110%)";
+    ctx.font = "130px phenomicon";
+    ctx.fillStyle = lengthchar[id].colorBg;
+    ctx.fillText(global.config.BOTNAME, 835, 340);
+    ctx.beginPath();
+  ////////////////////////////////////////
+   registerFont(__dirname + `/cache/UTM-Avo.ttf`, {
+      family: "UTM"
+    });
+    ctx.textAlign = "start";
+    ctx.font = "70px UTM";
+    ctx.fillStyle = "#000000";
+    ctx.fillText(`${z_1} : ${x_1} : ${y_1} `, 980, 440);
     ctx.restore();
     ctx.save();
+registerFont(__dirname + `/cache/CaviarDreams.ttf`, {
+      family: "time"
+    });
     ctx.textAlign = "start";
-    ctx.font = "250px Grandstander";
-    ctx.fillStyle = "#000"
-    ctx.fillText("anupx3aliya", 1200, 2750);
-    ctx.fillStyle = "#FFF"
-    ctx.fillText("anup.x3", 1600, 3150);
-    ctx.fillStyle = "#000"
-   ctx.fillText("rude larkaw", 2000, 3600);
+    ctx.font = "55px time";
+    ctx.fillText("Sagor", 930, 540)
+    ctx.fillText("Sagor", 930, 610)
+    ctx.fillText("Sagor", 930, 690)
+    ctx.restore();
+    ctx.save();
     ctx.beginPath();
     const imageBuffer = canvas.toBuffer();
-    fs.writeFileSync(pathImg, imageBuffer);
+   fs.writeFileSync(pathImg, imageBuffer);
   return api.sendMessage({
-    body: `==={ UPTIME ROBOT }===\nBot worked ${hours} hours ${minutes} minutes ${seconds} seconds\n\nTotal Users: ${global.data.allUserID.length}\nTotal Group : ${global.data.allThreadID.length}\nCPU in use: ${pidusage.cpu.toFixed(1)}%\nRam in use : ${byte2mb(pidusage.memory)}\nPing: ${Date.now() - timeStart}ms\nCharacter ID: ${id}\n━━━━━━━━━━━━━━\n[ ${timeNow} ]`,
+    body: `Server Information\n\nChip: ${chips}.\nProcessing Speed: ${speed}MHz.\n\nTotal Memory: ${byte2mb(os.totalmem())}.\nUsed: ${byte2mb(os.freemem())} (${(os.freemem() * 100 / os.totalmem()).toFixed()}%).\n\nBot is running up ${hours} hours ${minutes} minute ${seconds} seconds.\n\n❯ Total users: ${global.data.allUserID.length}\n❯ Total Group: ${global.data.allThreadID.length}\n❯ CPU in use: ${pidusage.cpu.toFixed(1)}%\n❯ Ram in use: ${byte2mb(pidusage.memory)}\n❯ Ping: ${Date.now() - timeStart}ms\n❯ Character ID: ${id}\n❯ Admin Faceboook:\n  https://www.facebook.com/SAGOR.DJK.FORYOU`,
     attachment: fs.createReadStream(pathImg)
   },
     event.threadID,
     () => fs.unlinkSync(pathImg),
     fs.unlinkSync(pathAva),
-    fs.unlinkSync(pathLine),
-    fs.unlinkSync(pathIconIG),    
-    fs.unlinkSync(pathIcongithub),
-    fs.unlinkSync(pathIcon),
     event.messageID
   );
-}
+	  }
