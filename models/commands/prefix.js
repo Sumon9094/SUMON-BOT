7fbfb6a4187c6f93ead011c72e9c131fb856bb17
SAGOR690beep <jahidul2782466@gmail.com>
2025-05-22 08:19:19 +0600

    Update prefix.js

--- models/commands/prefix.js
+++ models/commands/prefix.js
@@ -1,48 +1,91 @@
+const sendWaiting = true; // bật hoặc tắt gửi tin nhắn "đang tạo hình ảnh, vui ồng chờ đợi...";
+const textWaiting = "Đang khởi tạo hình ảnh, vui lòng chờ đợi trong giây lát";
+const fonts = "/cache/Play-Bold.ttf"
+const downfonts = "https://drive.google.com/u/0/uc?id=1uni8AiYk7prdrC7hgAmezaGTMH5R8gW8&export=download"
+const fontsLink = 20
+const fontsInfo = 28
+const colorName = "#00FFFF"
+
 module.exports.config = {
   name: "prefix",
-  version: "1.0.1",
+  version: "2.0.0",
   hasPermssion: 0,
-  credits: "SaGor",
-  description: "See the bot prefix",
-  commandCategory: "For admin",
+  credits: "CHORU-TIKTOKERS",
+  description: "Tạo card thông tin người dùng facebook",
+  commandCategory: "Nhóm",
   usages: "",
-  cooldowns: 5,
+  cooldowns: 10,
+  dependencies: {
+    canvas: "",
+    axios: "",
+    "fs-extra": "",
+  },
 };
 
-module.exports.handleEvent = async ({ event, api, Threads }) => {
-  var { threadID, messageID, body, senderID } = event;
-  //if (senderID == global.data.botID) return;
-  if ((this.config.credits) != "SaGor") { return api.sendMessage(`Changed credits!`, threadID, messageID)}
-  function out(data) {
-    api.sendMessage(data, threadID, messageID)
-  }
-  var dataThread = (await Threads.getData(threadID));
-  var data = dataThread.data; 
-  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
-
-  var arr = ["prefi","Prefix","prefix", "command mark", "What is the prefix of the bot?","command", "Command"];
-  arr.forEach(i => {
-    let str = i[0].toUpperCase() + i.slice(1);
-    if (body === i.toUpperCase() | body === i | str === body) {
-const prefix = threadSetting.PREFIX || global.config.PREFIX;
-      if (data.PREFIX == null) {
-        return out(`️️️️️️️️️️️️️️️️️️️️️️️️️This is my prefix⇉ [ ${prefix} ]`)
-      }
-      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️███████ ]▄▄▄▄▄▄▄▄\n▂▄▅█████████▅▄▃▂\nI███████████████████]\n◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤\n➢' + data.PREFIX)
-    }
-
-  });
-};
-
-module.exports.run = async({ event, api }) => {
-    return api.sendMessage("error", event.threadID)
+module.exports.circle = async (image) => {
+  const jimp = global.nodemodule["jimp"];
+  image = await jimp.read(image);
+  image.circle();
+  return await image.getBufferAsync("image/png");
 }
-️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️███████ ]▄▄▄▄▄▄▄▄\n▂▄▅█████████▅▄▃▂\nI███████████████████]\n◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤\n➢' + data.PREFIX)
-    }
+module.exports.run = async function ({ api, event, args, Users }) {
+  if ((this.config.credits) != "CHORU-TIKTOKERS") { return api.sendMessage(`⚡️Current credits have been changed`, event.threadID, event.messageID)}
+  let { senderID, threadID, messageID } = event;
+  const { loadImage, createCanvas } = require("canvas");
+  const request = require('request');
+  const fs = global.nodemodule["fs-extra"];
+  const axios = global.nodemodule["axios"];
+const time = process.uptime(),
+		hours = Math.floor(time / (60 * 60)),
+		minutes = Math.floor((time % (60 * 60)) / 60),
+		seconds = Math.floor(time % 60);
+	const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Dhaka").format("»D/MM/YYYY HH:mm:ss«");
+const timeStart = Date.now();
+  const Canvas = global.nodemodule["canvas"];
+  let pathImg = __dirname + `/cache/1.png`;
+  let pathAvata = __dirname + `/cache/2.png`;
+  /*                 */
+  let bg = (
+    await axios.get(encodeURI(`https://i.imgur.com/K692bGa.jpeg`), {
+      responseType: "arraybuffer",
+    })
+  ).data;
+  fs.writeFileSync(pathImg, Buffer.from(bg, "utf-8"));
 
-  });
+/*-----------------download----------------------*/
+if(!fs.existsSync(__dirname+`${fonts}`)) { 
+      let getfont = (await axios.get(`${downfonts}`, { responseType: "arraybuffer" })).data;
+       fs.writeFileSync(__dirname+`${fonts}`, Buffer.from(getfont, "utf-8"));
+    };
+/*---------------------------------------------*/
+
+  let baseImage = await loadImage(pathImg);
+  let canvas = createCanvas(baseImage.width, baseImage.height);
+  let ctx = canvas.getContext("2d");
+  ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+  Canvas.registerFont(__dirname+`${fonts}`, {
+        family: "Play-Bold"
+    });
+  ctx.font = `${fontsInfo}px Play-Bold`;
+  ctx.fillStyle = "#FFCC33";
+  ctx.textAlign = "start";
+  fontSize = 30;
+  ctx.font = `${fontsInfo}px Play-Bold`;
+  ctx.fillStyle = "#FFCC33";
+  ctx.textAlign = "start";
+  fontSize = 100;
+  ctx.fillText(`>>  Today is:   ${juswa}`, 70, 180);
+  ctx.fillText(`>>  Bot is up and running ${hours}:${minutes}:${seconds}`, 70, 230);
+  ctx.beginPath();
+  const imageBuffer = canvas.toBuffer();
+  fs.writeFileSync(pathImg, imageBuffer);
+  fs.removeSync(pathAvata);
+  
+  return api.sendMessage(
+    {body: `Prefix: ${global.config.PREFIX}\nToday is ${juswa}\nBot is up and running ${hours}:${mins}:${seconds}`, attachment: fs.createReadStream(pathImg) },
+    threadID,
+    () => fs.unlinkSync(pathImg),
+    messageID
+  );
 };
-
-module.exports.run = async({ event, api }) => {
-    return api.sendMessage("error", event.threadID)
-}
