6535323eceeb797c9b64d186b2356f2b0b231ec7
SAGOR690beep <jahidul2782466@gmail.com>
2025-05-22 08:09:52 +0600

    Update prefix.js

--- models/commands/prefix.js
+++ models/commands/prefix.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "prefix",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "SaGir",
+  credits: "SaGor",
   description: "See the bot prefix",
   commandCategory: "For admin",
   usages: "",
